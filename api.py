6636fbfe373b37df26d9117f38e5c64db2367ec8
HariK-developer <hariswdeveloper@gmail.com>
2024-07-14 10:10:40 +0530

    2fa completed

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-from fastapi import Depends, HTTPException, APIRouter
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi import Depends, HTTPException, APIRouter, Request
+from fastapi.responses import JSONResponse
 import pyotp
 import qrcode
 from database import get_db
@@ -7,7 +7,13 @@ from models import User
 from schema import  UserLogin, UserSignup
 from sqlalchemy.orm import Session
 import bcrypt
-import io
+import os
+from datetime import datetime, timedelta
+import jwt
+
+SECRET_KEY = "f3c5e57ef88a6c4e7eb5fa53bb03ab11b7281b9b3d74f90ad1e9650b70727dbf"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1  
 
 router = APIRouter()
 
@@ -35,7 +41,7 @@ async def signup(signup_request: UserSignup, db: Session = Depends(get_db)):
     )
 
 @router.post("/login")
-async def login(login_request: UserLogin, db: Session = Depends(get_db)):
+async def login(login_request: UserLogin, request: Request, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == login_request.username).first()
     if not user or not bcrypt.checkpw(
         login_request.password.encode("utf-8"), user.password.encode("utf-8")
@@ -49,7 +55,7 @@ async def login(login_request: UserLogin, db: Session = Depends(get_db)):
         if not login_request.otp:
             raise HTTPException(
                 status_code=401,
-                detail={"status": False, "message": "OTP required"},
+                detail={"status": False, "otp": True, "message": "OTP required"},
             )
         totp = pyotp.TOTP(user.totp_secret)
         if not totp.verify(login_request.otp):
@@ -57,7 +63,10 @@ async def login(login_request: UserLogin, db: Session = Depends(get_db)):
                 status_code=401,
                 detail={"status": False, "message": "Invalid OTP"},
             )
-        return JSONResponse(content={"status": True, "message": "Login successful"})
+        # Generate JWT token
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+        return JSONResponse(content={"status": True, "message": "Login successful", "access_token": access_token})
     else:
         # 2FA is not enabled, generate a QR code for enabling 2FA
         totp = pyotp.TOTP(pyotp.random_base32())
@@ -68,14 +77,38 @@ async def login(login_request: UserLogin, db: Session = Depends(get_db)):
         db.refresh(user)
 
         # Generate provisioning URI
-        uri = totp.provisioning_uri(name=user.username, issuer_name="App")
+        uri = totp.provisioning_uri(name=user.username, issuer_name=user.username)
 
         # Generate QR code
         qr = qrcode.make(uri)
-        buf = io.BytesIO()
-        qr.save(buf, format="PNG")
-        buf.seek(0)
+        file_path = os.path.join("static", "qrcodes", f"{user.username}.png")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        qr.save(file_path)
 
-        return StreamingResponse(buf, media_type="image/png")
+        # Construct the full URL for the QR code
+        qr_code_url = request.url_for('static', path=f'qrcodes/{user.username}.png')
+
+        # Generate JWT token
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+
+        return JSONResponse(content={
+            "status": True,
+            "message": "Login successful",
+            "otp": False,
+            "qr_code_url": str(qr_code_url),  # Convert URL object to string
+            "access_token": access_token
+        })
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 
 
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from database import Base, engine
 from api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Mount the static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#  add cors middleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
